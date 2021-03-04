0ffdf54ec09f133e49085f38286b27f943a8f2fa
ArturBelous <insomnerbel@gmail.com>
2021-03-04 11:52:30 +0200

    new commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+//const letter = confirm( "are u here?");
+//console.log(letter);
+
+//const answer = +prompt ("сколько лет?", "20");
+//console.log(answer + 10);
+
+const answers = [];
+//  answers[0] = prompt("как ваше имя?", "");
+//  answers[1] = prompt("как ваша фамилия?", "");
+//  answers[2] = prompt("как ваше отчество?", "");
+
+//  console.log(typeof(answers));
+const sex = "men";
+console.log(`I am a ${sex}`);
+
+const user = "Artur";
+alert(`Hello, ${user}`);
+console.log('arr' + ' - object');
+console.log(4 + 5);
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+console.log(incr, decr);
+
+console.log(8%3);
+
+const isChecked = true,
+    isClose = true;
+
+    console.log(isChecked && isClose);
+
+const brose = prompt("АРтур?");
+console.log(brose);
